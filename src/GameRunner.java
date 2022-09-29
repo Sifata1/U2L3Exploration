320d7ab83212ef2e55ce450e8765d3b4ad0e0a78
BT_4N2_26 <sanan6576@bths.edu>
2022-09-29 12:07:21 -0400

    wasd

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,10 +1,22 @@
 public class GameRunner {
     public static void main(String[] args) {
         Game game = new Game();
+
         game.addPlayer();
         game.addPlayer();
-        System.out.println("There are " + game.getPlayers() + "players!");
-        System.out.println("Are you ready to begin?");
+        System.out.println("There are " + game.getPlayers() + " players!");
+        System.out.println();
+
+        game.increaseScore(10);
+        game.increaseScore(15);
+        game.addPlayer();
+        game.increaseScore(25);
+
+        System.out.println("<------------------------------>");
+        System.out.println("Players: " + game.getPlayers());
+        System.out.println("Total Score: " + game.getScore());
+        System.out.println("Average Score Per Player: " + game.averageScorePerPlayer());
+        System.out.println("<------------------------------>");
 
 
     }
